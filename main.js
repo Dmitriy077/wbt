95ef120ac99d29806e53ef4d9678b7b212a6ece2
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-09-15 03:28:06 +0500

    14.09

--- main.js
+++ main.js
@@ -12,4 +12,26 @@ $(function() {
 		prevArrow:'<div class="news_arrows_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
 		nextArrow:'<div class="news_arrows_next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
 	});
+	
+	$('.filters_article_slider').slick({
+		// autoplay: true,
+  // 		autoplaySpeed: 3000,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+		adaptiveWidth: true,
+		variableWidth: true,
+		appendArrows:'.filters_arrows',
+		prevArrow:'<div class="filters_arrows_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+		nextArrow:'<div class="filters_arrows_next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+	});
+
+	$('.main_slider').slick({
+		// autoplay: true,
+  // 		autoplaySpeed: 3000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+	});
 });
