feffddcf460fb9102b4afffb473f46385fc103f3
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-09-16 18:07:32 +0500

    16.09/1

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 $(function() {
 	$('.news_slider_slick').slick({
 		// autoplay: true,
-  // 		autoplaySpeed: 3000,
+  		// autoplaySpeed: 3000,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		dots: false,
@@ -19,7 +19,7 @@ $(function() {
 	//Слайдер товаров главная
 	$('.filters_article_slider').slick({
 		// autoplay: true,
-  // 		autoplaySpeed: 3000,
+  		// autoplaySpeed: 3000,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		dots: false,
@@ -34,7 +34,7 @@ $(function() {
 	// Главный слайдер
 	$('.main_slider').slick({
 		// autoplay: true,
-  // 		autoplaySpeed: 3000,
+  		// autoplaySpeed: 3000,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: false,
@@ -43,7 +43,7 @@ $(function() {
 	// Слайдер акксессуаров в карточке товара
 	$('.productCard_accessories_slider').slick({
 		// autoplay: true,
-  // 		autoplaySpeed: 3000,
+  		// autoplaySpeed: 3000,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		dots: false,
@@ -58,7 +58,7 @@ $(function() {
 	// Слайдер товаров в карчтоке товара
 	$('.otherFiltrationSystems_slider').slick({
 		// autoplay: true,
-  // 		autoplaySpeed: 3000,
+  		// autoplaySpeed: 3000,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		dots: false,
@@ -69,9 +69,35 @@ $(function() {
 		prevArrow:'<div class="otherFiltrationSystems_arrows_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
 		nextArrow:'<div class="otherFiltrationSystems_arrows_next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
 	});
+
+
+	//Медиа слайдер акссесуаров в системах фильтрации
+	$('.accessories_list_slider').slick({
+		// autoplay: true,
+  		// autoplaySpeed: 3000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: false,
+		adaptiveWidth: true,
+		variableWidth: true,
+		// appendArrows:'.otherFiltrationSystems_arrows',
+		// prevArrow:'<div class="otherFiltrationSystems_arrows_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+		// nextArrow:'<div class="otherFiltrationSystems_arrows_next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+	});
 });
 
 //
+
+
+
+// Медиа запрос для слайдера
+
+if(window.matchMedia('(max-width: 500px)').matches){
+	$('.accessories_list').addClass('accessories_list_slider');
+	} else {
+		$('.accessories_list').removeClass('accessories_list_slider');
+	}
 
 
 
